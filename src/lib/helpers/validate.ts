2ad1d4498e1ca21dc8f73e7d46d1e315bf56bfa5
chientrm <chientrm@gmail.com>
2023-06-30 18:34:49 +0000

    yeh boi

--- src/lib/helpers/validate.ts
+++ src/lib/helpers/validate.ts
@@ -1,16 +1,16 @@
 import * as yup from 'yup';
 
 const validate = async <TShape extends yup.ObjectShape>(
-	request: Request,
-	spec: TShape
+  request: Request,
+  spec: TShape
 ) => {
-	const formData = await request.formData(),
-		object = Object.keys(spec).reduce(
-			(a, b) => ({ ...a, [b]: formData.get(b) }),
-			{}
-		),
-		result = yup.object(spec).validate(object);
-	return result;
+  const formData = await request.formData(),
+    object = Object.keys(spec).reduce(
+      (a, b) => ({ ...a, [b]: formData.get(b) }),
+      {}
+    ),
+    result = yup.object(spec).validate(object);
+  return result;
 };
 
 export { validate };
